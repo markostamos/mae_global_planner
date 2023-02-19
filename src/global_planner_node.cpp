9a43321de3922b0304e522af8a19781ce8683c6d
markostamos <gimarkostamos@gmail.com>
2023-02-19 19:38:57 +0200

    parallel plan

--- src/global_planner_node.cpp
+++ src/global_planner_node.cpp
@@ -4,6 +4,7 @@
 #include <mae_global_planner/PlanService.h>
 #include <mae_global_planner/GlobalPlanService.h>
 #include <mae_global_planner/utils.h>
+#include <thread>
 class GlobalPlannerNode
 {
 private:
@@ -46,36 +47,32 @@ private:
     }
     bool globalPlanCallback(mae_global_planner::GlobalPlanService::Request &req, mae_global_planner::GlobalPlanService::Response &res)
     {
-        GlobalPlanner global_planner_ = GlobalPlanner(nh_private_.param("population_size", 50),
-                                                      nh_private_.param("tournament_size", 10),
-                                                      nh_private_.param("mutation_rate", 0.6),
-                                                      nh_private_.param("p2opt_rate", 0.3),
-                                                      nh_private_.param("social_disaster_rate", 0.1));
-
         ROS_INFO("Make Global Plan service called");
         try
         {
             std::vector<Point> targets = toPoints(req.targets);
             std::vector<Point> starting_positions = toPoints(req.starting_positions);
-
             std::vector<Point> cluster_centroids = starting_positions;
-
             std::vector<std::vector<Point>> cluster_points = GlobalPlanner::kmeans(targets, cluster_centroids.size(), cluster_centroids, 1000, 0.01);
+            res.global_plan.resize(cluster_centroids.size());
+
+            std::vector<std::thread> exec_threads;
+
             for (int i = 0; i < cluster_points.size(); i++)
             {
-                if (!cluster_points[i].empty())
-                {
-                    global_planner_.initialize(cluster_points[i], cluster_points[i].size(), starting_positions[i]);
-                    global_planner_.evolve(req.timeout_ms / cluster_centroids.size(), false);
-                    res.global_plan.push_back(toPointArray(global_planner_.getBestPath()));
-                }
-                else
-                {
-                    mae_global_planner::PointArray empty_plan;
-                    empty_plan.points.push_back(toGeometryPoint(starting_positions[i]));
-                    res.global_plan.push_back(empty_plan);
-                }
+                exec_threads.push_back(std::thread(&GlobalPlannerNode::calculateAgentPlan,
+                                                   this,
+                                                   cluster_points[i],
+                                                   starting_positions[i],
+                                                   req.timeout_ms,
+                                                   i,
+                                                   std::ref(res)));
             }
+
+            // join threads
+
+            for (auto &t : exec_threads)
+                t.join();
 
             return true;
         }
@@ -84,6 +81,27 @@ private:
         {
             ROS_ERROR("Make Global Plan service failed: %s", e.what());
             return false;
+        }
+    }
+
+    void calculateAgentPlan(const std::vector<Point> &targets, const Point &starting_position, const int &timeout_ms, const int index, mae_global_planner::GlobalPlanService::Response &res)
+    {
+        GlobalPlanner global_planner_ = GlobalPlanner(nh_private_.param("population_size", 50),
+                                                      nh_private_.param("tournament_size", 10),
+                                                      nh_private_.param("mutation_rate", 0.6),
+                                                      nh_private_.param("p2opt_rate", 0.3),
+                                                      nh_private_.param("social_disaster_rate", 0.1));
+        if (!targets.empty())
+        {
+            global_planner_.initialize(targets, targets.size(), starting_position);
+            global_planner_.evolve(timeout_ms, true);
+            res.global_plan[index] = toPointArray(global_planner_.getBestPath());
+        }
+        else
+        {
+            mae_global_planner::PointArray empty_plan;
+            empty_plan.points.push_back(toGeometryPoint(starting_position));
+            res.global_plan[index] = empty_plan;
         }
     }
 };
